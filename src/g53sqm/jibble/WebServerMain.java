b18d691200cb92fc017e3d1d4083462671be48f2
PC <PC@hp>
2015-12-10 07:57:30 +0800

    Seventeenth Commit -Fixed directory path

---
+++ src/g53sqm/jibble/WebServerMain.java
@@ -0,0 +1,59 @@
+package g53sqm.jibble;
+
+import java.io.IOException;
+
+/* 
+Copyright Paul James Mutton, 2001-2004, http://www.jibble.org/
+
+This file is part of Jibble Web Server / WebServerLite.
+
+This software is dual-licensed, allowing you to choose between the GNU
+General Public License (GPL) and the www.jibble.org Commercial License.
+Since the GPL may be too restrictive for use in a proprietary application,
+a commercial license is also provided. Full license information can be
+found at http://www.jibble.org/licenses/
+
+$Author: pjm2 $
+$Id: WebServerMain.java,v 1.2 2004/02/01 13:37:35 pjm2 Exp $
+
+*/
+
+
+/**
+ * This class contains the main method for the Jibble Web Server.
+ * 
+ * @author Copyright Paul Mutton, http://www.jibble.org/
+ */
+public class WebServerMain {
+
+    public static void main(String[] args) throws IOException{
+        
+        String rootDir = WebServerConfig.directory.getCanonicalPath();
+        int port = WebServerConfig.DEFAULT_PORT;
+        
+        if (args.length > 0) {
+            rootDir = WebServerConfig.directory.getCanonicalPath() +"\\"+args[0];
+        }
+        
+        if (args.length > 1) {
+            try {
+                port = Integer.parseInt(args[1]);
+            }
+            catch (NumberFormatException e) {
+                // Stick with the default value.
+            }
+        }
+        
+        try {
+            WebServer server = new WebServer(rootDir, port);
+            System.out.println("Jibble Web Server (modified by Ong Peng Chee for G53SQM)");
+            System.out.println("Root Directory: " + rootDir);
+            System.out.println("Port:" + port);
+            server.activate();
+        }
+        catch (WebServerException e) {
+            System.out.println(e.toString());
+        }        
+    }
+
+}
